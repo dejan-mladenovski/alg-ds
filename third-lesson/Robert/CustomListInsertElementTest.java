63833845f58f2304949ffb2e48e420ea438e3cf1
DIFFERENT-5\Administrator <robert@different.mk>
2016-12-22 18:48:10 +0100

    Robert's commit

---
+++ third-lesson/Robert/CustomListInsertElementTest.java
@@ -0,0 +1,92 @@
+package test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import main.main.CustomList;
+
+/**
+ * Created by dmladeno on 20.12.2016.
+ */
+@RunWith(BlockJUnit4ClassRunner.class)
+public class CustomListInsertElementTest {
+
+
+  @Test
+  public void shouldInsertElementInList() {
+    // given
+    CustomList<Integer> testee = new CustomList<Integer>();
+    Integer elementToInsert = 1;
+    // when
+    testee.add(elementToInsert);
+    // then
+    assertEquals(1, testee.size());
+    assertEquals(elementToInsert, testee.get(0));
+
+//    uncomment the line to see a failed test. No element exists at index 1
+   // assertEquals(elementToInsert, testee.get(1));
+  }
+  
+  @Test
+  public void shouldInsertStringElementInList() {
+    // given
+    CustomList<String> testee = new CustomList<String>();
+    String elementToInsert = "Stefan";
+    // when
+    testee.add(elementToInsert);
+    // then
+    assertEquals(1, testee.size());
+    assertEquals(elementToInsert, testee.get(0));
+  }
+
+  @Test
+  public void shouldInsertTwoStringElementsInList() {
+    // given
+    CustomList<String> testee = new CustomList<String>();
+    String elementToInsert1 = "Stefan";
+    String elementToInsert2 = "Robert";
+    String elementToInsert3 = "Maja";
+    // when
+    testee.add(elementToInsert1);
+    testee.add(elementToInsert2);
+    testee.add(elementToInsert3);
+    // then
+    assertEquals(3, testee.size());
+    assertEquals(elementToInsert1, testee.get(0));
+    assertEquals(elementToInsert2, testee.get(1));
+    assertEquals(elementToInsert3, testee.get(2));
+  }
+  
+  
+  @Test
+  public void shouldNotInsertNullList() {
+    // given
+    CustomList<String> testee = new CustomList<String>();
+
+    // when
+    testee.add(null);
+    // then
+    assertEquals(0, testee.size());
+    assertNull(testee.get(0));
+  }
+  
+  @Test
+  public void shouldInsertATousendList() {
+    // given
+    CustomList<Double> testee = new CustomList<Double>();
+	Random random = new Random();
+    // when
+    for (int i = 0; i < 1000; i++) {
+    	testee.add(random.nextDouble());
+	}
+    // then
+    assertEquals(1000, testee.size());
+    assertNotNull(testee.get(999));
+  }
+  
+}
---
+++ third-lesson/Robert/CustomListRemoveElementTest.java
@@ -0,0 +1,147 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import main.main.CustomList;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class CustomListRemoveElementTest {
+
+	  @Test
+	  public void shouldRemoveTheFirstElementOfList() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    String elementToInsert2 = "Robert";
+		    String elementToInsert3 = "Maja";
+		    // when
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert2);
+		    testee.add(elementToInsert3);
+		    testee.remove(elementToInsert1);
+		    // then
+		    assertEquals(2, testee.size());
+		    assertEquals(elementToInsert2, testee.get(0));
+		    assertEquals(elementToInsert3, testee.get(1));
+	  }
+	
+	  @Test
+	  public void shouldRemoveTheLastElementOfList() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    String elementToInsert2 = "Robert";
+		    String elementToInsert3 = "Maja";
+		    // when
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert2);
+		    testee.add(elementToInsert3);
+		    testee.remove(elementToInsert3);
+		    // then
+		    assertEquals(2, testee.size());
+		    assertEquals(elementToInsert1, testee.get(0));
+		    assertEquals(elementToInsert2, testee.get(1));
+	  }
+	  
+	  @Test
+	  public void shouldRemoveTheLastElementOfListByIndex() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    String elementToInsert2 = "Robert";
+		    String elementToInsert3 = "Maja";
+		    // when
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert2);
+		    testee.add(elementToInsert3);
+		    testee.removeAt(2);
+		    // then
+		    assertEquals(2, testee.size());
+		    assertEquals(elementToInsert1, testee.get(0));
+		    assertEquals(elementToInsert2, testee.get(1));
+	  }
+	  
+	  @Test
+	  public void shouldRemoveTheRandomElementOfListByIndex() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    String elementToInsert2 = "Robert";
+		    String elementToInsert3 = "Maja";
+		    // when
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert2);
+		    testee.add(elementToInsert3);
+		    testee.removeAt(1);
+		    // then
+		    assertEquals(2, testee.size());
+		    assertEquals(elementToInsert1, testee.get(0));
+		    assertEquals(elementToInsert3, testee.get(1));
+	  }
+	
+	  @Test
+	  public void shouldRemoveElementOfListOutOfBoundsByIndex() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    String elementToInsert2 = "Robert";
+		    String elementToInsert3 = "Maja";
+		    // when
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert2);
+		    testee.add(elementToInsert3);
+		    testee.removeAt(5);
+		    // then
+		    assertEquals(3, testee.size());
+	  }
+	  
+	  @Test
+	  public void shouldRemoveElementOfListWithDataNull() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    String elementToInsert2 = "Robert";
+		    String elementToInsert3 = "Maja";
+		    // when
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert2);
+		    testee.add(elementToInsert3);
+		    testee.remove(null);
+		    // then
+		    assertEquals(3, testee.size());
+	  }
+	  
+	  @Test
+	  public void shouldRemoveElementOfEmptyList() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    // when
+		    testee.remove(elementToInsert1);
+		    // then
+		    assertEquals(0, testee.size());
+	  }
+	  
+	  @Test
+	  public void shouldRemoveElementOfListWithDuplicates() {
+		    // given
+		    CustomList<String> testee = new CustomList<String>();
+		    String elementToInsert1 = "Stefan";
+		    String elementToInsert2 = "Robert";
+		    String elementToInsert3 = "Maja";
+		    // when
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert1);
+		    testee.add(elementToInsert2);
+		    testee.add(elementToInsert3);
+		    testee.remove(elementToInsert1);
+		    // then
+		    assertEquals(2, testee.size());
+	  }
+}
